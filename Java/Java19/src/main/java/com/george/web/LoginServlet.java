46d8bad1f5b807020905658c6e2eddfa03a2d529
Poole <2112978551@qq.com>
2025-07-22 22:55:06 +0800

    CookieAndSession_CheckCode add Filter_Listener case commit

--- Java/Java19/src/main/java/com/george/web/LoginServlet.java
+++ Java/Java19/src/main/java/com/george/web/LoginServlet.java
@@ -17,7 +17,6 @@ public class LoginServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
 
-
         //获取页面传入的数据
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -38,36 +37,27 @@ public class LoginServlet extends HttpServlet {
                 //创建Cookie
                 Cookie c_username = new Cookie("username", username);
                 Cookie c_password = new Cookie("password", password);
-
                 //设置存活时间
                 c_username.setMaxAge(60*60*24);
                 c_password.setMaxAge(60*60*24);
-
                 //发送Cookie
                 response.addCookie(c_username);
                 response.addCookie(c_password);
             }
-
             //将用户信息存储到Session
             HttpSession session = request.getSession();
             session.setAttribute("user", userResult);
-
             //动态获取路径
             String contextPath = request.getContextPath();
-
             //并且重定向到查询页面
-            response.sendRedirect(contextPath+ "/selectAllServlet");
+            response.sendRedirect(contextPath+ "/index.html");
         }else {
             //登入失败
-
             //存储错误信息到request
             request.setAttribute("login_msg", "用户名或密码错误！");
-
             //跳转，转发到login.jsp页面
             request.getRequestDispatcher("login.jsp").forward(request, response);
         }
-
-
     }
 
     @Override
