374af417457f14ffd9d76e48839e4d020f12893a
Poole <2112978551@qq.com>
2025-08-02 16:55:41 +0800

    springMVC 获取request数据

--- SpringMVC/SpringMVC04/src/main/java/com/george/controller/UserController.java
+++ SpringMVC/SpringMVC04/src/main/java/com/george/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.george.controller;
 
-import jakarta.servlet.http.HttpServlet;
+import com.george.pojo.User;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
@@ -55,6 +53,82 @@ public class UserController {
         System.out.println(Arrays.toString(interest));
         System.out.println(info);
 
+        return "ok";
+    }
+
+    /**
+     * 第三种方式：依靠控制器方法上的形参名来接收
+     *         如果 请求参数名 和 控制器方法上的形参名 保持一致，那么 @RequestParam注解可以省略。
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @param sex 性别
+     * @param interest 兴趣
+     * @param info 简介
+     * @return "ok"
+     */
+    @PostMapping("/reg03")  // @RequestParam 会自动进行 类型转换
+    public String _register(
+            String username,
+            String password,
+            Integer sex,
+            String[] interest,
+            String info
+    ){
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(sex);
+        System.out.println(Arrays.toString(interest));
+        System.out.println(info);
+
+        return "ok";
+    }
+
+    /**
+     * 第四种方式：使用POJO类/JavaBean接收请求参数(这是最常用的)
+     *         底层实现原理：反射机制。
+     *         不过，使用这种方式的前提是：POJO类的属性名必须和请求参数的参数名保持一致
+     *
+     * @param user pojo类
+     * @return "ok"
+     */
+    @PostMapping("/reg04")  // @RequestParam 会自动进行 类型转换
+    public String register(User user){
+
+        System.out.println(user);
+
+        return "ok";
+    }
+
+    /**
+     * 使用 @RequestHeader 注解，它用来标注 形参。
+     *      作用是：将  请求头信息  映射到   控制器方法的形参 上。
+     *
+     * @param user pojo
+     * @param host 请求头中的 host 字段
+     * @return "ok"
+     */
+    @PostMapping("/reg05")  // @RequestParam 会自动进行 类型转换
+    public String register(User user,
+                           @RequestHeader(value = "host", required = false, defaultValue = "null") String host){
+
+        System.out.println(user);
+        System.out.println(host);
+
+        return "ok";
+    }
+
+    /**
+     * 使用 @CookieValue 注解标注控制器方法上的形参。
+     *     获取客户端提交的Cookie
+     * @return "ok"
+     */
+    @RequestMapping("/reg06")
+    public String register_(User user,
+                           @CookieValue(value = "id", required = false, defaultValue = "")
+                           String id){
+        System.out.println(user);
+        System.out.println("客户端提交过来的cookie，它的值是：" + id);
         return "ok";
     }
 }
--- SpringMVC/SpringMVC04/src/main/java/com/george/pojo/User.java
+++ SpringMVC/SpringMVC04/src/main/java/com/george/pojo/User.java
@@ -1,4 +1,76 @@
 package com.george.pojo;
 
+import java.util.Arrays;
+
 public class User {
+    private String username;
+    private String password;
+    private Integer sex;
+    private String[] interest;
+    private String info;
+
+
+    public User() {
+    }
+
+    public User(String username, String password, Integer sex, String[] interest, String info) {
+        this.username = username;
+        this.password = password;
+        this.sex = sex;
+        this.interest = interest;
+        this.info = info;
+    }
+
+
+    /**
+     * 设置
+     * @param username 用户名
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+    /**
+     * 设置
+     * @param password 密码
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    /**
+     * 设置
+     * @param sex 性别
+     */
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+
+
+    /**
+     * 设置
+     * @param interest 兴趣
+     */
+    public void setInterest(String[] interest) {
+        this.interest = interest;
+    }
+
+
+
+    /**
+     * 设置
+     * @param info 简介
+     */
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String toString() {
+        return "User{username = " + username + ", password = " + password + ", sex = " + sex + ", interest = " + Arrays.toString(interest) + ", info = " + info + "}";
+    }
 }
